6b31a6154787c5b64979e7d77a5c2d7cd673baa1
David Felix <davidffelix@hotmail.com>
2022-05-03 21:17:50 -0300

    Correction

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.blueGrey,
-          appBar: AppBar(
-            backgroundColor: Colors.blueGrey[900],
-            title: Text('Eu Sou Rico',
-              textAlign: TextAlign.center,
-            ),
-          ),
-          body: Center(
-            child: Image(
-              image: AssetImage('imagens/rubi.png'),
-              alignment: Alignment.center,
-              ),
-          ),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text(
+            'Eu Sou Rico',
           ),
         ),
-      );
+        body: Center(
+          child: Image(
+            image: AssetImage('imagens/rubi.png'),
+            alignment: Alignment.center,
+          ),
+        ),
+      ),
+    ),
+  );
 }
